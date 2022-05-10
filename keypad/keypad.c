a43f8a0ae877d70e1eddbcc0d475a06a4da09c1e
Kollaly <abdelrahmanelkollaly@gmail.com>
2022-05-10 15:46:03 +0200

    KeyPad Read

--- keypad/keypad.c
+++ keypad/keypad.c
@@ -35,3 +35,40 @@ void KEYPAD_INIT(void)
     GPIO_enableInternalResistance(PORTA_ID, PIN3_ID, PULL_UP);
 }
 
+/*
+ * Description :
+ * Read the Keypad.
+ * Register(s) : GPIO_DATA.
+ */
+
+uint8_t KEYPAD_READ(void)
+{
+    uint8_t array[4][4] = 
+    {
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'}
+    };
+    uint8_t row, column, x;
+    uint8_t return_value =ERROR_VALUE; // NO_INPUTS FROM KEYPAD
+    for(row=0; row<4; row++)
+    {
+        GPIO_writePort(PORTA_ID, 0XFF);
+        GPIO_writePin(PORTA_ID , row+4, 0);
+        for(column=0; column<4; column++)
+        {
+            x=GPIO_readPin(PORTA_ID ,column);
+            if(x==0)
+            {
+                return_value = array[row][column];
+                break;
+            }
+        }
+        if(x==0)
+        {
+            break;  
+        }      
+    }
+    return return_value;
+}
