7b515e9ef1d8a5cc750ba2993dc21ff5b17fbbf3
Esraa Amr <39887130+esraaAmr@users.noreply.github.com>
2022-05-11 14:17:22 +0200

    Update lcd.h

--- LCD/lcd.h
+++ LCD/lcd.h
@@ -28,3 +28,40 @@ void LCD_init(void);
  * Register(s) : GPIO_DATA.
  */
 void LCD_COMMAND(char command);
+/*
+ * Description :
+ * Sending The pulse.
+ * Register(s) : CTRL_PORT.
+ */
+
+static void sendPulse(void);
+/*
+ * Description :
+ * Sending Char.
+ * Register(s) : CTRL_PORT,DATA_PORT.
+ */
+
+void send_Char(char char);
+/*
+ * Description :
+ * Sending String.
+ * Register(s) : CTRL_PORT,DATA_PORT.
+ */
+
+void send_String(char *data);
+/*
+ * Description :
+ * Clear Screen.
+ * Register(s) : Clear_Screen.
+ */
+
+void LCD_Clear_Screen();
+/*
+ * Description :
+ * Move Curser.
+ * Register(s) : C_FIRST_LINE,C_SEC_LINE.
+ */
+
+void LCD_Move_Curser(char row,char col);
+
+#endif /* LCD_H_ */
