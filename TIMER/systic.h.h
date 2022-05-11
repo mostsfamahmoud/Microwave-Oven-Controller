fa078065e8f9f5263b17cba4acddde1f6533b99e
Mostafa Mahmoud <72917420+mostsfamahmoud@users.noreply.github.com>
2022-05-11 15:10:45 +0200

    Delete TIMER directory

--- TIMER/systic.h.h
+++
@@ -1,3 +0,0 @@
-#define NVIC_ST_CTRL_R       (*((volatile unsigned long *)0xE000E010))
-#define NVIC_ST_RELOAD_R         (*((volatile unsigned long *)0xE000E014))
-#define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))
