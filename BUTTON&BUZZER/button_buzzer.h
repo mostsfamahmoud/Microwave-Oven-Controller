2462f48b91ad656999fca45e66fdad4ec517f0b5
Huda <104650236+Huda480@users.noreply.github.com>
2022-05-12 02:24:21 +0200

    Update button_buzzer.h

---
+++ BUTTON&BUZZER/button_buzzer.h
@@ -0,0 +1,25 @@
+
+/**************************
+ *
+ * Module: BUTTON&BUZZER
+ *
+ * File Name: button_buzzer.h
+ *
+ * Description: Button and Buzzer header file
+ *
+ * Authors: Abdelrahman Ali Mohamed Ali
+			Huda Abdelnasser Taalap Haridy
+ *
+ ***************************/
+/*
+ * Description :
+ * initalization the Button Pin.
+ * 
+ */
+void BUTTON_INIT(void);
+/*
+ * Description :
+ * Function that reads the Button.
+ * 
+ */
+uint8_t BUTTON_READ (void);
