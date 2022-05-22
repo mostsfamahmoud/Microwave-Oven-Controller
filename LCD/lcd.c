c2e25a6c2256d21340a7bf0637c8ff007dc21c1e
sherinsameh <104653051+sherinsameh@users.noreply.github.com>
2022-05-23 00:13:10 +0200

    Update lcd.c

--- LCD/lcd.c
+++ LCD/lcd.c
@@ -1,119 +1,58 @@
-/***********************
+/******************************************************************************
  *
  * Module: LCD
  *
  * File Name: LCD.c
  *
- * Description: Source file for THE LCD Driver.
- * 
- * Authors: Sherin Sameh Abd El-Samad Ali 
- *          Esraa Amr Abdelmoneam Hassan Kandil
- *         
- ***************************/
-#include "gpio.h"
-#include "Macros.h"
-#include "std_types.h"
-#include "LCD_Macros.h"
-#include "timer.h"
-/***************************
- *                       Functions Definitions                             *
- ***************************/
+ * Description: source file for The LCD Driver.
+ *
+ * Author: Sherin Sameh Abd El-Samad Ali
+ *         Esraa Amr Abdelmoneam Hassan Kandil
+ *******************************************************************************/
+
+#include "lcd.h"
+
+/*******************************************************************************
+ *                           Functions Definitions                             *
+ *******************************************************************************/
 
 /*
  * Description :
- * Initialization the required port for the LCD.
- * Register(s) : GPIO_DIR , GPIO_DATA 
+ * Initialization of the LCD.
+ * Register(s) : GPIO_DIR , GPIO_DATA
  */
-void LCD_init(void)
+
+
+void LCD_INIT(void)
 {
-PORT_INIT(DATA_PORT); 
-PORT_INIT(CTRL_PORT);
- GPIO_setPortDirection(DATA_PORT, 0xFF);
- GPIO_setPinDirection(CTRL_PORT, Enable,1);
- GPIO_setPinDirection(CTRL_PORT, RS, 1);
- GPIO_setPinDirection(CTRL_PORT, RW, 1);
- GPIO_writePin(CTRL_PORT, RW, 0);
- LCD_COMMAND(LCD_8BITS_M);
- Generic_delay_m_sec(1);
- LCD_COMMAND(DIS_ON_C_OFF);
- Generic_delay_m_sec(1);
- LCD_COMMAND(Clear_Screen);
- Generic_delay_m_sec(10);
-LCD_COMMAND(ENTRY_MODE);
-Generic_delay_m_sec(1);
-}
-/*
- * Description :
- * Initialization the required port for the Keypad.
- * Register(s) : GPIO_DATA
- */
-void LCD_COMMAND(char command){
- GPIO_writePin(DATA_PORT,command);
-    GPIO_writePin(CTRL_PORT,RS,0);
-    sendPulse();
+    GPIO_Init(PORTA_ID);
+    GPIO_Init(PORTB_ID);
+    GPIO_setPortDirection(DATA_PORT, 0xFF);
+    GPIO_setPinDirection(CTRL_PORT, Enable, PIN_OUTPUT);
+    GPIO_setPinDirection(CTRL_PORT, RS, PIN_OUTPUT);
+    lcd_command(Function_set_8bit);
     Generic_delay_m_sec(1);
-}
-/*
- * Description :
- * Sending The pulse.
- * Register(s) : CTRL_PORT.
- */
-
-static void sendPulse(void){
-    GPIO_writePin(CTRL_PORT,Enable,1);
-    Generic_delay_m_sec(2);
-    GPIO_writePin(CTRL_PORT,Enable,0);
+    lcd_command(moveCursorRight); // set to entry mode
+    Generic_delay_m_sec(1);
+    lcd_command(cursorBlink); //  display is on and cursor blinking
+    Generic_delay_m_sec(1);
+    lcd_command(clear_display); // clear screen
+    Generic_delay_m_sec(2)
+    lcd_command(returnHome); // return home
     Generic_delay_m_sec(2);
 }
-
 /*
  * Description :
- * Sending Char.
- * Register(s) : CTRL_PORT,DATA_PORT.
+ * send commands to LCD.
+ * Register(s) : GPIO_DATA.
  */
-
-void send_Char(char char){
-    GPIO_writePin(DATA_PORT,char);
-    GPIO_writePin(CTRL_PORT,RS,1);
-    sendPulse();
+void lcd_command(unsigned char cmd)
+{
+    GPIO_writePort(DATA_PORT, cmd);
+    GPIO_writePort(CTRL_PORT, 0x08);
     Generic_delay_m_sec(1);
-}
-/*
- * Description :
- * Sending String.
- * Register(s) : CTRL_PORT,DATA_PORT.
- */
-void send_String(char *data){
-    while((*data)!='0'){
-        send_Char((*data));
-        data++;
-    }
-}
-/*
- * Description :
- * Clear Screen.
- * Register(s) : Clear_Screen.
- */
+    GPIO_writePort(CTRL_PORT, 0x00); 
+    Generic_delay_m_sec(50);
 
-void LCD_Clear_Screen(){
-    LCD_COMMAND(Clear_Screen);
-    Generic_delay_m_sec(10);
-}
-/*
- * Description :
- * Move Curser.
- * Register(s) : C_FIRST_LINE,C_SEC_LINE.
- */
-
-void LCD_Move_Curser(char row,char col){
-    char position =0;
-    if(row=0){
-        position = (C_FIRST_LINE)+col-1;
-    }else if(row =2){
-        position = (C_SEC_LINE)+col-1;
-    }else{
-        position = C_FIRST_LINE;
-    }
-    LCD_COMMAND(position);
-    Generic_delay_m_sec(1);
+    return;
 }
