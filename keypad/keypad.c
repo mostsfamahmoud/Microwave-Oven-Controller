db51234e58e1adda11d30d1f11b5a912bc653d2c
Abdelrahman Ali <68446289+Kollaly@users.noreply.github.com>
2022-05-22 23:31:46 +0200

    keypad initialization

--- keypad/keypad.c
+++ keypad/keypad.c
@@ -9,10 +9,7 @@
  * Authors: Huda Abdelnasser Taalap Haridy 
  *          Abdelrahman Ali Mohamed Ali
  ***************************/
-#include "gpio.h"
-#include "Macros.h"
-#include "tm4c123gh6pm.h"
-#include "std_types.h"
+
 #include "keypad.h"
 
 /***************************
@@ -22,53 +19,31 @@
 /*
  * Description :
  * Initialization the required port for the Keypad.
- * Register(s) : GPIO_PUR, GPIO_DIR.
  */
 
-void KEYPAD_INIT(void)
-{
-    PORT_INIT(PORTA_ID); 
-    GPIO_setPortDirection(PORTA_ID, 0XF0); // ROWS>> outputs , columns>> inputs
-    GPIO_enableInternalResistance(PORTA_ID, PIN0_ID, PULL_UP);
-    GPIO_enableInternalResistance(PORTA_ID, PIN1_ID, PULL_UP);
-    GPIO_enableInternalResistance(PORTA_ID, PIN2_ID, PULL_UP);
-    GPIO_enableInternalResistance(PORTA_ID, PIN3_ID, PULL_UP);
-}
-
-/*
- * Description :
- * Read the Keypad.
- * Register(s) : GPIO_DATA.
- */
-
-uint8_t KEYPAD_READ(void)
-{
-    uint8_t array[4][4] = 
+uint8_t array[4][4] =
     {
         {'1', '2', '3', 'A'},
         {'4', '5', '6', 'B'},
         {'7', '8', '9', 'C'},
         {'*', '0', '#', 'D'}
     };
-    uint8_t row, column, x;
-    uint8_t return_value =ERROR_VALUE; // NO_INPUTS FROM KEYPAD
-    for(row=0; row<4; row++)
-    {
-        GPIO_writePort(PORTA_ID, 0XFF);
-        GPIO_writePin(PORTA_ID , row+4, 0);
-        for(column=0; column<4; column++)
-        {
-            x=GPIO_readPin(PORTA_ID ,column);
-            if(x==0)
-            {
-                return_value = array[row][column];
-                break;
-            }
-        }
-        if(x==0)
-        {
-            break;  
-        }      
-    }
-    return return_value;
+
+/*
+ * Description :
+ * Initialization the required port for the Keypad.
+ */
+
+void KEYPAD_INIT(void)
+{
+
+    GPIO_Init(PORTE_ID);
+    GPIO_Init(PORTA_ID);
+    GPIO_setPortDirection(PORTE_ID, 0x0F);
+    GPIO_PORTA_PDR_R |= 0xF0;
+    GPIO_PORTA_DATA_R &= 0xF0;
+    GPIO_PORTE_DATA_R &=0X0F;
+	
+	
 }
+
