cc43f338d542e6c6d9acabceb53c0befc15b9f7b
Esraa Amr <39887130+esraaAmr@users.noreply.github.com>
2022-05-23 00:17:10 +0200

    Update lcd.c

--- LCD/lcd.c
+++ LCD/lcd.c
@@ -56,3 +56,58 @@ void lcd_command(unsigned char cmd)
 
     return;
 }
+/*
+ * Description :
+ * Sending Char.
+ * Register(s) : CTRL_PORT,DATA_PORT.
+ */
+void send_char(unsigned char data) //-----> recive only one char
+{
+    GPIO_writePort(DATA_PORT, data);
+    // GPIO_PORTB_DATA_R=data;
+    GPIO_writePort(CTRL_PORT, 0x0C);
+    // GPIO_PORTE_DATA_R=0X05;
+    Generic_delay_m_sec(1);
+    GPIO_writePort(CTRL_PORT, 0x04);
+    // GPIO_PORTE_DATA_R=0X01;
+    Generic_delay_m_sec(50);
+
+    return;
+}
+/*
+ * Description :
+ * Sending String.
+ * Register(s) : CTRL_PORT,DATA_PORT.
+ */
+void send_String(unsigned char *data)
+{
+    while (*data)
+    {
+        send_char(*data);
+        data++;
+    }
+}
+/*
+ * Description :
+ * move cursor to write data on preffered row and column
+ * Register(s) : CTRL_PORT,DATA_PORT.
+ */
+void LCD_Move_Curser(unsigned char row, unsigned char col)
+{
+    unsigned char position = 0;
+
+    if (row == 1)
+    {
+        position = (FirstRow) + col - 1;
+    }
+    else if (row == 2)
+    {
+        position = (SecondRow) + col - 1;
+    }
+    else
+    {
+        position = FirstRow;
+    }
+    lcd_command(position);
+    Generic_delay_m_sec(1);
+}
