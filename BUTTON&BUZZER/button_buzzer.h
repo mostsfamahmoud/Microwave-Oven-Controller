1f95b1c65d2cd557e8c5ab20574b8422f8a7955e
Huda <104650236+Huda480@users.noreply.github.com>
2022-05-23 00:19:20 +0200

    Buzzer functions

--- BUTTON&BUZZER/button_buzzer.h
+++ BUTTON&BUZZER/button_buzzer.h
@@ -1,43 +1,42 @@
-
-/**************************
+/**
  *
  * Module: BUTTON&BUZZER
  *
  * File Name: button_buzzer.h
  *
- * Description: Button and Buzzer header file
+ * Description: Button and Buzzer Header file
  *
  * Authors: Abdelrahman Ali Mohamed Ali
-			Huda Abdelnasser Taalap Haridy
+            Huda Abdelnasser Taalap Haridy
  *
- ***************************/
+ **/
+#ifndef Button_BuzzerH
+#define Button_BuzzerH
 
-#include "std_types.h"
+#include "gpio.h"
 
-/*
- * Description :
- * initalization the Buzzer Pin.
- * 
- */
+/**
+ *                              Functions Prototypes                           *
+ **/
 
-void Buzzer_init(uint8_t portNum , uint8_t pinNum);
+/ Description :
 
-	/*
- * Description :
- * calling the Buzzer Pin.
- * 
- */
-	
-void Buzzer_call();
-/*
- * Description :
- * initalization the Button Pin.
- * 
- */
-void BUTTON_INIT(void);
-/*
- * Description :
- * Function that reads the Button.
- * 
- */
-uint8_t BUTTON_READ (void);
+    Initialization of Buzzer 
+
+/
+
+void Buzzer_init(void);
+
+/ Description :
+    Setting the buzzer ON 
+/
+
+void Buzzer_ON(void);
+
+/ Description :
+    Setting the buzzer OFF 
+/
+
+void Buzzer_OFF(void);
+
+/
