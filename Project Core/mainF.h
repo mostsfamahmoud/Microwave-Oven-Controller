3996b7ff5803ec63274fbff3c27403d23aa80b8a
sherinsameh <104653051+sherinsameh@users.noreply.github.com>
2022-05-24 22:47:10 +0200

    Update mainF.h

--- Project Core/mainF.h
+++ Project Core/mainF.h
@@ -7,3 +7,41 @@
  * Description: Header file for The Main functions 
  *
  *******************************************************************************/
+#ifndef MAINF_H_
+#define MAINF_H_
+
+#include "lcd.h"
+#include "keypad.h"
+#include "button_buzzer.h"
+#include "portF.h"
+
+
+/*
+ * Description:
+ *    This Function implements State A in our projects.
+ */
+
+void state_A();
+
+/*
+ * Description:
+ *    This Function implements State B in our projects.
+ */
+
+void state_B();
+
+/*
+ * Description:
+ *    This Function implements State C in our projects.
+ */
+
+void state_C();
+
+/*
+ * Description:
+ *    This Function implements State D in our projects.
+ */
+
+void state_D();
+
+#endif
