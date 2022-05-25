969c6a4b540e5f017d4307f5101eb7b041e853da
Huda <104650236+Huda480@users.noreply.github.com>
2022-05-25 14:53:52 +0200

    Update std_types.h

--- GPIO/std_types.h
+++ GPIO/std_types.h
@@ -30,6 +30,11 @@ typedef unsigned char boolean;
 
 #define NULL_PTR    ((void*)0)
 
+typedef unsigned char         uint8_t;          //           0 .. 255              
+typedef unsigned short        uint16_t;         //           0 .. 65535            
+typedef unsigned long         uint32_t;         //           0 .. 4294967295       
+typedef unsigned long long    uint64_t;         //       0 .. 18446744073709551615
+
 typedef signed char           sint8_t;          /*        -128 .. +127             */
 typedef signed short          sint16_t;         /*      -32768 .. +32767           */
 typedef signed long           sint32_t;         /* -2147483648 .. +2147483647      */
